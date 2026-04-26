dd6edd26c4b9bbd4e7dd1762d7d9c21abbefb21a
Reed Evertt <reed@evertt.com>
2026-04-26 00:31:25 -0700

    Polished page design & outlined content

--- content/js/script.js
+++ content/js/script.js
@@ -1,1 +1,15 @@
-// empty for now!
+// Just de-blur the background the further down you go;
+// make it look just a little more professional!
+const MAX_BLUR_SIZE = 6.0;
+function setBlur(t) {
+	document.documentElement.style.setProperty("--blur-size", `${t * MAX_BLUR_SIZE}px`);
+}
+function calcScrollProg(element) {
+	return (element.scrollTop / (element.scrollHeight - element.clientHeight));
+}
+
+scroller = document.getElementById("main-scroller");
+scroller.addEventListener("scroll", (event) => {
+	setBlur(1.0 - calcScrollProg(scroller));
+});
+setBlur(1.0 - calcScrollProg(scroller));
