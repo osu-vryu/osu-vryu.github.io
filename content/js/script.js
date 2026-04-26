af5e76188736f20129f322aea4fff28f4ba74ad8
Reed Evertt <reed@evertt.com>
2026-04-26 15:08:14 -0700

    Finalized first version of the VRyu website

--- content/js/script.js
+++ content/js/script.js
@@ -1,8 +1,8 @@
 // Just de-blur the background the further down you go;
 // make it look just a little more professional!
-const MAX_BLUR_SIZE = 6.0;
+const MAX_BLUR_SIZE = 8.0;
 function setBlur(t) {
-	document.documentElement.style.setProperty("--blur-size", `${t * MAX_BLUR_SIZE}px`);
+	document.documentElement.style.setProperty("--blur-size", `${(t * 0.75 + 0.25) * MAX_BLUR_SIZE}px`);
 }
 function calcScrollProg(element) {
 	return (element.scrollTop / (element.scrollHeight - element.clientHeight));
